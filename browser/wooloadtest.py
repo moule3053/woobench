60d7bab3f044ce42ad81a832f2dcba9191cd6d6c
Mulugeta Ayalew Tamiru <mulugeta.a.tamiru@campus.tu-berlin.de>
2017-12-23 01:16:45 +0100

    Update reporting

--- browser/wooloadtest.py
+++ browser/wooloadtest.py
@@ -2,6 +2,12 @@ from locust import HttpLocust, TaskSet, task
 import string
 import random
 import time
+from locust.web import app
+
+from src import report
+
+# For reporting
+app.add_url_rule('/htmlreport', 'htmlreport', report.download_report)
 
 class UserBehavior(TaskSet):
     def on_start(self):
